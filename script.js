7d91b74cf24f4eb5cd8a02cae1fedb7247862210
NN <Dv-nn-01@mail.ru>
2021-09-06 11:29:43 +0300

    dinamic digital clock

---
+++ script.js
@@ -0,0 +1,30 @@
+let section = document.querySelector('section'),
+    icons = document.querySelector('.icons');
+
+   icons.onclick = () => {
+      section.classList.toggle('dark');
+};
+   
+// creating function and calling it in every seconds
+
+setInterval(() => {
+   let date = new Date(),
+      hour = date.getHours(),
+      min = date.getMinutes(),
+      sec = date.getSeconds();
+   
+   let timeDay;
+   timeDay = hour < 12 ? 'AM' : 'PM';
+   hour = hour > 12 ? hour - 12 : hour;
+   hour = hour == 0 ? hour = 12 : hour;
+
+   hour = hour < 10 ? '0' + hour : hour;
+   min = min < 10 ? '0' + min : min;
+   sec = sec < 10 ? '0' + sec : sec;
+
+   document.querySelector('.hour-num').innerText = hour;
+   document.querySelector('.min-num').innerText = min;
+   document.querySelector('.sec-num').innerText = sec;
+   document.querySelector('.am-pm').innerText = timeDay;
+
+}, 1000);
